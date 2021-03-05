74b875acaa89225410363504e5e823056fae790b
Michal Szwajgier <michalszwajgier@gmail.com>
2021-03-05 17:40:20 +0100

    Change logic for game

--- js/script.js
+++ js/script.js
@@ -1,43 +1,29 @@
-let computerChoice = Math.floor(Math.random() * 3 + 1);
-
-let computerMove = '';
-
-if (computerChoice == '1') {
-  computerMove = 'kamień';
-} else if (computerChoice == '2') {
-  computerMove = 'papier';
-} else if (computerChoice == '3') {
-  computerMove = 'nożyce';
-} else {
-  computerMove = 'nieznany ruch';
+function getMoveName(argMoveId) {
+  if (argMoveId == 1) {
+    return 'kamień';
+  } else if (argMoveId == 2) {
+    return 'papier';
+  } else if (argMoveId == 3) {
+    return 'nożyce';
+  } else {
+    printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+    return 'nieznany ruch';
+  }
 }
 
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+function displayResult(argComputerMove, argPlayerMove) {
+  printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
 
-let playerMove = '';
-
-if (playerInput == '1') {
-  playerMove = 'kamień';
-} else if (playerInput == '2') {
-  playerMove = 'papier';
-} else if (playerInput == '3') {
-  playerMove = 'nożyce';
-} else {
-  playerMove = 'nieznany ruch';
+  if (argPlayerMove == argComputerMove) {
+    printMessage('Remis!');
+  } else if (argPlayerMove == 'kamień' && argComputerMove == 'nożyce' || argPlayerMove == 'papier' && argComputerMove == 'kamień' || argPlayerMove == 'nożyce' && argComputerMove == 'papier') {
+    printMessage('Wygrywasz rundę!');
+  } else {
+    printMessage('Komputer był lepszy!');
+  }
 }
 
-let winner = '';
+let computerMove = getMoveName(Math.floor(Math.random() * 3 + 1));
+let playerMove = getMoveName(prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.'));
 
-if (playerInput == '1' && computerChoice == '1' || playerInput == '2' && computerChoice == '2' || playerInput == '3' && computerChoice == '3') {
-  winner = 'Remis!';
-} else if (playerInput == '1' && computerChoice == '3' || playerInput == '2' && computerChoice == '1' || playerInput == '3' && computerChoice == '2') {
-  winner = 'Wygrywasz rundę!';
-} else {
-  winner = 'Komputer był lepszy!';
-}
-
-console.log('Twój ruch to: ' + playerMove);
-console.log('Komputer wybrał: ' + computerMove);
-console.log(`Komputer wybrał ${computerMove}, Ty wybrałeś ${playerMove}. ${winner}`);
-
-printMessage('Twój ruch to: ' + playerMove);
+displayResult(computerMove, playerMove);
